ecca9d5db2083ac798aad36c8ca269d6d9af15e3
Harapriya <harapriyamohanty270@gmail.com>
2026-03-09 00:47:38 +0530

    files added to src folder

---
+++ client/src/pages/connections.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const connections = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default connections
---
+++ client/src/pages/createpost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const createpost = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default createpost
