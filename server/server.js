29303ab965489e704e55ffa910ebfcdb5992e916
Harapriya <harapriyamohanty270@gmail.com>
2026-05-02 13:49:22 +0530

    error rectified

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 4000;
 
-if (!process.env.VERCEL) {
+if (!process.env.VERCEL !== "1") {
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
 
